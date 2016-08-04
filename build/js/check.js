1e201c17a37ec00731bb0cdc5af0c31a8bd75609
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-04 23:23:30 +0300

    Задание 6-1

---
+++ build/js/check.js
@@ -0,0 +1,35 @@
+var getMessage = function () {
+    document.write(getMessage);
+}
+    var a = function (typeof(a)==Boolean) {  
+        return true;}
+if (a(typeof(a)==Boolean)) {
+    getMessage('Я попал в [b]');
+    } else {
+    getMessage('Я никуда не попал'); 
+    }            
+        var a = function (typeof(a)==Number) {
+            return true; 
+        }
+if (a(typeof(a)==Number)) {
+    getMessage('Я прыгнул на [a] * 100 сантиметров');
+}
+        
+         var a = function (a instanceof Array) {
+                     return true; }
+         var numberOfSteps = a+b;
+            if (a(a instanceof Array)) {
+                getMessage('Я прошёл [numberOfSteps] шагов');
+            }
+            var a,b = function (a,b instanceof Array) {
+                     return true; }     
+           if (a,b(a,b instanceof Array)) {
+               getMessage('Я прошёл [distancePath] метров');
+               var distancePath=0;
+         for(var i=0, i<=a.length,i++)
+         {
+             distancePath=distancePath+a[i]*b[i];
+         }
+      
+}
+
