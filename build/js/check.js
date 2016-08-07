70acbf2e6f7d4d571663ee4c02527a97981efaf5
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-07 23:22:31 +0300

    Задание6-3

--- build/js/check.js
+++ build/js/check.js
@@ -1,29 +1,33 @@
 function getMessage(a,b) {
- if (typeof(a) === 'boolean') {
-    //вот сюда мы попадем если это boolean
+    if (typeof(a) === 'boolean') {
+    
     if (a) {
-        //вот сюда мы попадем, если он true
+       
         return  'Я попал в [b]';
     } else {
-        //сюда попадем, если a===false, т.е. не true
+        
         return 'Я никуда не попал';
-}
-}
-if (typeof(a) === 'number') {
-  return 'Я прыгнул на [a] * 100 сантиметров';
-}
-var numberOfSteps = 0;
-var distancePath = 0;
-  if (a != (typeof(a) === 'boolean') & a != (typeof(a) === 'number'))  {
-   for (var i = 0; i < a.length; i++) {
-   numberOfSteps += a[i];
+    }
+    }
+    if (typeof(a) === 'number') {
+       return 'Я прыгнул на [a] * 100 сантиметров';
+    }
+    
+    var numberOfSteps = 0;
+    var distancePath = 0;
+    
+    if (a != (typeof(a) === 'boolean') & a != (typeof(a) === 'number'))  {
+      for (var i = 0; i < a.length; i++) {
+       numberOfSteps += a[i];
    }
-   if (b != (typeof(a) === 'boolean') & b != (typeof(a) === 'number')) {
-   for (var i = 0; i < b.length; i++) {
-distancePath += a[i] * b[i];}
+        
+    if (b != (typeof(a) === 'boolean') & b != (typeof(a) === 'number')) {
+      for (var i = 0; i < b.length; i++) {
+        distancePath += a[i] * b[i];
+      }
     return 'Я прошёл ' + distancePath + ' метров';
    } 
-   return ' Я прошёл ' + numberOfSteps + ' шагов';
+    return ' Я прошёл ' + numberOfSteps + ' шагов';
    }
 }
 
