0c2b278c20b98830839a193b43753428067cc307
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-07 16:53:43 +0300

    Задание6-2

--- build/js/check.js
+++ build/js/check.js
@@ -1,35 +1,29 @@
-var getMessage = function () {
-    document.write(getMessage);
-}
-    var a = function (typeof(a)==Boolean) {  
-        return true;}
-if (a(typeof(a)==Boolean)) {
-    getMessage('Я попал в [b]');
+function getMessage(a,b) {
+ if (typeof(a) === 'boolean') {
+    //вот сюда мы попадем если это boolean
+    if (a) {
+        //вот сюда мы попадем, если он true
+        return  'Я попал в [b]';
     } else {
-    getMessage('Я никуда не попал'); 
-    }            
-        var a = function (typeof(a)==Number) {
-            return true; 
-        }
-if (a(typeof(a)==Number)) {
-    getMessage('Я прыгнул на [a] * 100 сантиметров');
+        //сюда попадем, если a===false, т.е. не true
+        return 'Я никуда не попал';
 }
-        
-         var a = function (a instanceof Array) {
-                     return true; }
-         var numberOfSteps = a+b;
-            if (a(a instanceof Array)) {
-                getMessage('Я прошёл [numberOfSteps] шагов');
-            }
-            var a,b = function (a,b instanceof Array) {
-                     return true; }     
-           if (a,b(a,b instanceof Array)) {
-               getMessage('Я прошёл [distancePath] метров');
-               var distancePath=0;
-         for(var i=0, i<=a.length,i++)
-         {
-             distancePath=distancePath+a[i]*b[i];
-         }
-      
+}
+if (typeof(a) === 'number') {
+  return 'Я прыгнул на [a] * 100 сантиметров';
+}
+var numberOfSteps = 0;
+var distancePath = 0;
+  if (a != (typeof(a) === 'boolean') & a != (typeof(a) === 'number'))  {
+   for (var i = 0; i < a.length; i++) {
+   numberOfSteps += a[i];
+   }
+   if (b != (typeof(a) === 'boolean') & b != (typeof(a) === 'number')) {
+   for (var i = 0; i < b.length; i++) {
+distancePath += a[i] * b[i];}
+    return 'Я прошёл ' + distancePath + ' метров';
+   } 
+   return ' Я прошёл ' + numberOfSteps + ' шагов';
+   }
 }
 
