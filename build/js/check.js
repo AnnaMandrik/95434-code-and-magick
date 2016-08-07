6f8cb98537868dfbabd77d8571422b75a6da2c4a
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-08 00:58:31 +0300

    Задание6-4

--- build/js/check.js
+++ build/js/check.js
@@ -1,33 +1,27 @@
 function getMessage(a,b) {
     if (typeof(a) === 'boolean') {
-    
-    if (a) {
-       
-        return  'Я попал в [b]';
-    } else {
-        
+      if (a) {
+        return  'Я попал в ' + b;
+      } else {
         return 'Я никуда не попал';
-    }
+      }
     }
     if (typeof(a) === 'number') {
-       return 'Я прыгнул на [a] * 100 сантиметров';
+       return 'Я прыгнул на ' + a * 100 + ' сантиметров';
     }
-    
     var numberOfSteps = 0;
     var distancePath = 0;
-    
-    if (a != (typeof(a) === 'boolean') & a != (typeof(a) === 'number'))  {
-      for (var i = 0; i < a.length; i++) {
-       numberOfSteps += a[i];
-   }
-        
-    if (b != (typeof(a) === 'boolean') & b != (typeof(a) === 'number')) {
-      for (var i = 0; i < b.length; i++) {
-        distancePath += a[i] * b[i];
-      }
-    return 'Я прошёл ' + distancePath + ' метров';
-   } 
-    return ' Я прошёл ' + numberOfSteps + ' шагов';
-   }
+    if (Array.isArray(a)) {
+        for (var i = 0; i < a.length; i++) {
+          numberOfSteps += a[i];
+        }  
+        if (Array.isArray(b)) {
+             for (var i = 0; i < b.length; i++) {
+             distancePath += a[i] * b[i];
+             }
+           return 'Я прошёл ' + distancePath + ' метров';
+          } 
+     return ' Я прошёл ' + numberOfSteps + ' шагов';
+     }
 }
 
