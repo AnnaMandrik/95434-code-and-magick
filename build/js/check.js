008fa6df991f7bf4c534cae2053354cb948b0741
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-08 23:05:01 +0300

    Задание6-5

--- build/js/check.js
+++ build/js/check.js
@@ -1,27 +1,35 @@
-function getMessage(a,b) {
-    if (typeof(a) === 'boolean') {
-      if (a) {
-        return  'Я попал в ' + b;
-      } else {
-        return 'Я никуда не попал';
-      }
+function getMessage(a, b) {
+    if (typeof (a) === 'boolean') {
+        if (a) {
+            return 'Я попал в ' + b;
+        } else {
+            return 'Я никуда не попал';
+        }
     }
-    if (typeof(a) === 'number') {
-       return 'Я прыгнул на ' + a * 100 + ' сантиметров';
+    if (typeof (a) === 'number') {
+        return 'Я прыгнул на ' + a * 100 + ' сантиметров';
     }
     var numberOfSteps = 0;
     var distancePath = 0;
     if (Array.isArray(a)) {
-        for (var i = 0; i < a.length; i++) {
-          numberOfSteps += a[i];
-        }  
         if (Array.isArray(b)) {
-             for (var i = 0; i < b.length; i++) {
-             distancePath += a[i] * b[i];
-             }
-           return 'Я прошёл ' + distancePath + ' метров';
-          } 
-     return ' Я прошёл ' + numberOfSteps + ' шагов';
-     }
+            var LengthA = a.length;
+            var LengthB = b.length;
+            var LengthArray;
+            if (LengthA < LengthB) {
+                LengthArray = LengthA;
+            } else {
+                LengthArray = LengthB;
+            }
+            for (var i = 0; i < LengthArray; i++) {
+                distancePath += a[i] * b[i];
+            }
+            return 'Я прошёл ' + distancePath + ' метров';
+        }
+        for (var i = 0; i < a.length; i++) {
+            numberOfSteps += a[i];
+        }
+        return 'Я прошёл ' + numberOfSteps + ' шагов';
+    }
 }
 
