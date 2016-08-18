daede278f029ad99ea15dc77ea33582138cfb130
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-08-18 19:20:11 +0300

    Задание 13-1

--- src/form.js
+++ src/form.js
@@ -1,8 +1,10 @@
 'use strict';
 
+
 window.form = (function() {
   var formContainer = document.querySelector('.overlay-container');
   var formCloseButton = document.querySelector('.review-form-close');
+
 
   var form = {
     onClose: null,
@@ -23,12 +25,42 @@ window.form = (function() {
       }
     }
   };
+  var name= document.querySelector('#review-name','value');
+  var text = document.querySelector('#review-text');
+  var nameFields = document.querySelector('.review-fields-name');
+  var markFirst = document.querySelector('#review-mark-1');
+  var markSecond = document.querySelector('#review-mark-2');
+  var formControls = document.querySelector('.review-fields');
+  var buttons = document.querySelector('.review-submit');
 
+  function validateOnSubmit() {
+    var valid = true;
+    if (valid) {
+      if (name) {
+        name.setAttribute('value','');
+        name.setAttribute('required','required');
+        nameFields.style.display = 'none';
+      }
+      if (markFirst.getAttribute('checked') || markSecond.getAttribute('checked')){
+        text.setAttribute('value','');
+        text.setAttribute('required','required');
+        formControls.style.display = 'none';
+      }
+    }else {
+      result (false);
+      buttons.setAttribute('disabled', 'disabled');
+    }
+  }
+   validateOnSubmit();
+  name.onchange = validateOnSubmit;
+  text.onchange = validateOnSubmit;
 
   formCloseButton.onclick = function(evt) {
     evt.preventDefault();
     form.close();
   };
 
+
   return form;
 })();
+
