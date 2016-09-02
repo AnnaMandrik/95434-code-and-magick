a49180903f0e0c20719ef2158f08608e3ca40c9b
Anna Mandrik <AnnaMandrik1984@yandex.ru>
2016-09-02 23:30:31 +0300

    Задание 17-2 Исправление по замечаниям

--- src/reviews.js
+++ src/reviews.js
@@ -8,14 +8,6 @@ var elementToClone = (reviewTemplate.content || reviewTemplate).querySelector('.
 
 reviewsFilter.classList.add('invisible');
 
-/**if ('content' in reviewTemplate) {
- +  elementToClone = reviewTemplate.content.querySelector('.review');
- +} else {
- +  elementToClone = reviewTemplate.querySelector('.review');
- +}**/
-function parseReviews() {
-  console.log(reviews);
-}
 var createCallback = function(url, callback) {
   window.JSONPCallback = function(data) {
     if (typeof callback === 'function') {
@@ -28,23 +20,23 @@ var createCallback = function(url, callback) {
 };
 createCallback('http://localhost:1506/api/reviews', function(data) {
   reviews = data;
-  parseReviews(reviews);
   reviews.forEach(function(review) {
     showFeedback(review, reviewList);
   });
 });
 function showFeedback(review, container) {
   var element = elementToClone.cloneNode(true);
+  var reviewAuthor = element.querySelector('.review-author');
   var WIDTH_ONE_STAR = 40;
   var IMAGE_LOAD_TIMEOUT = 10000;
   var authorPictureTimeout;
   var authorPicture = new Image(124, 124);
-  element.querySelector('.review-author').title = review.author.name;
-  element.querySelector('.review-author').alt = review.author.name;
+  reviewAuthor.title = review.author.name;
+  reviewAuthor.alt = review.author.name;
   element.querySelector('.review-rating').style.width = (review.rating * WIDTH_ONE_STAR) + 'px';
   authorPicture.onload = function(event) {
     clearTimeout(authorPictureTimeout);
-    element.querySelector('.review-author').src = event.target.src;
+    reviewAuthor.src = event.target.src;
   };
 
   authorPicture.onerror = function() {
